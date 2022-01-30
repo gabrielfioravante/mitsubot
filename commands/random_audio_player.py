ea963515dff6ccc52fbe4c57d5bd9319ca5e430c
Gabriel Fioravante Mônego <61801407+gfmonego@users.noreply.github.com>
2022-01-30 02:54:07 -0300

    feat: remove instance when voice chanel is empty

--- commands/random_audio_player.py
+++ commands/random_audio_player.py
@@ -27,13 +27,13 @@ class commandInstance:
            await self.ctx.author.voice.channel.connect()
            self.ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
 
-           await self.ctx.send('Now playing: {}'.format(audio_file))
-
            while self.ctx.voice_client.is_playing():
              sleep(1)
            await self.ctx.voice_client.disconnect()
 
-    def cog_unload(self):
+    async def cog_unload(self, custom_message):
+        if custom_message:
+            await self.ctx.send(custom_message)
         self.play.cancel()
 
 
@@ -42,6 +42,17 @@ class randomAudioPlayer(commands.Cog):
         self.bot = bot
         self.instances = []
     
+    async def remove_instance(self, channel_id, custom_message):
+        for instance in self.instances:
+            if instance.channel_id == channel_id:
+                await instance.cog_unload(custom_message)
+                self.instances.remove(instance)
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, *_):
+            if before.channel is not None and not member.bot:
+                if len(before.channel.members) == 0:
+                    await self.remove_instance(before.channel.id, 'Não há ninguém no canal. Vou descansar ;)')
 
     @commands.command()
     async def start(self, ctx):
@@ -57,10 +68,4 @@ class randomAudioPlayer(commands.Cog):
 
     @commands.command()
     async def stop(self, ctx):
-        for instance in self.instances:
-            if instance.channel_id == ctx.author.voice.channel.id:
-                instance.cog_unload()
-                self.instances.remove(instance)
-                await ctx.send("Tá bom, eu paro")
-
-
+        await self.remove_instance(ctx.author.voice.channel.id, 'Parando áudios aleatórios no canal')
