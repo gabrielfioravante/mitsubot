0ca0c37aa594d72e0c3c38ce8b654fb278d4cc0b
Gabriel Fioravante Mônego <61801407+gfmonego@users.noreply.github.com>
2022-02-04 19:35:14 -0300

    refactor: rename some variables/methods/classes and extract method

--- commands/random_audio_player.py
+++ commands/random_audio_player.py
@@ -2,7 +2,7 @@ import discord, os, random, asyncio
 # from time import sleep
 from discord.ext import commands, tasks
 
-class commandInstance:
+class playerRoutine:
     def __init__(self, channel_id, ctx, channel):
         self.channel_id = channel_id
         self.ctx = ctx
@@ -48,41 +48,44 @@ class commandInstance:
 class randomAudioPlayer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.instances = []
+        self.routines = []
     
-    async def remove_instance(self, channel_id, custom_message):
-        for instance in self.instances:
-            if instance.channel_id == channel_id:
-                await instance.cog_unload(custom_message)
-                self.instances.remove(instance)
-
-    @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, *_):
-            if before.channel is not None and not member.bot:
-                if len(before.channel.members) == 0:
-                    await self.remove_instance(before.channel.id, 'Não há ninguém no canal. Vou descansar ;)')
-
-    async def custom_interval_parser(self, instance: commandInstance, ctx, custom_interval: float):
+    async def custom_interval_parser(self, instance: playerRoutine, ctx, custom_interval: float):
        if custom_interval != 20.0:
            if custom_interval > 0:
                instance.set_custom_interval(custom_interval)
            else:
                await ctx.send("Formato de tempo inválido. Utilize número positivos...\n Utilizando intervalo padrão (20 minutos)")
 
+    async def create_routine(self, ctx, custom_interval):
+        channel_id = ctx.author.voice.channel.id
+        channel = ctx.author.voice.channel
+        if not any(i.channel_id == channel_id for i in self.routines):
+            new_routine = playerRoutine(channel_id, ctx, channel)
+            await self.custom_interval_parser(new_routine, ctx, custom_interval)
+            self.routines.append(new_routine)   
+        else:
+            await ctx.send("Eu já fui iniciado, amigão")
+
+    async def remove_routine(self, channel_id, custom_message):
+        for instance in self.routines:
+            if instance.channel_id == channel_id:
+                await instance.cog_unload(custom_message)
+                self.routines.remove(instance)
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, *_):
+            if before.channel is not None and not member.bot:
+                if len(before.channel.members) == 0:
+                    await self.remove_routine(before.channel.id, 'Não há ninguém no canal. Vou descansar ;)')
+
     @commands.command()
     async def start(self, ctx, custom_interval: float=20.0):
         if ctx.author.voice:
-            channel_id = ctx.author.voice.channel.id
-            channel = ctx.author.voice.channel
-            if not any(i.channel_id == channel_id for i in self.instances):
-                new_instance = commandInstance(channel_id, ctx, channel)
-                await self.custom_interval_parser(new_instance, ctx, custom_interval)
-                self.instances.append(new_instance)   
-            else:
-                await ctx.send("Eu já fui iniciado, amigão")
+            await self.create_routine(ctx, custom_interval)
         else:
             await ctx.send("Irmão, você tem que estar conectado a algum canal de voz!")
 
     @commands.command()
     async def stop(self, ctx):
-        await self.remove_instance(ctx.author.voice.channel.id, 'Parando áudios aleatórios no canal')
+        await self.remove_routine(ctx.author.voice.channel.id, 'Parando áudios aleatórios no canal')
